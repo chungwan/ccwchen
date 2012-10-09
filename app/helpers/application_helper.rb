032b26e8392d71ddde18c3134f28ea6d3618c0cf
Christine Chen <chungwan@yahoo.com>
2012-10-09 00:33:13 -0700

    rough draft

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Christine Chen's Resume"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
